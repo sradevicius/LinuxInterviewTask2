40e0888e88f20ae4a6c0b1a208890724dc88bfb7
Simon <sradevicius1@gmail.com>
2022-05-03 14:54:41 +0300

    Last commit deleted a bit too much

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import sys
 from requests.auth import HTTPBasicAuth
 import datetime
 
+
 def send_mail():
     pass
+
+
+def get_latest_push_date(user, token):
+    auth = HTTPBasicAuth(user, token)
+    endpoint = 'https://api.github.com/users/torvalds/events/public'
+    response = requests.get(endpoint, auth=auth)
+    for i in range(len(response.json())):
+        if response.json()[i]['type'] == 'PushEvent':
+            return response.json()[i]['created_at']
+
 
 def get_datetime_from_date_string(date):
     date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
@@ -15,11 +26,7 @@ def main():
     latest_commit_date = None
 
     date = get_latest_push_date(sys.argv[1], sys.argv[2])
-    date = get_datetime_from_date_string(date)
     print(date)
-    ## Some dates returned are far in the future, i.e. 2099 or 2038
-    ## I also don't really care about commits done before 2022, so the below code
-    ## Should get rid of those future commits in response
     pass
 
 
