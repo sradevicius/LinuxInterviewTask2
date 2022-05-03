903c9f54585ca640aa2fc12e7000f81201b746d9
Simon <sradevicius1@gmail.com>
2022-05-03 11:35:28 +0300

    Realized easier way to do this is via search API, changed get_data function to use it instead

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ import sys
 from requests.auth import HTTPBasicAuth
 import datetime
 
-def get_data(user, passwd):
-    auth = HTTPBasicAuth(user, passwd)
-    endpoint = 'https://api.github.com/graphql'
-    query = """query {
-      user(login: "torvalds"){
-        contributionsCollection {
-          endedAt
-        }
-      }
-    }
-    """
+#def get_data(user, passwd):
+#    auth = HTTPBasicAuth(user, passwd)
+#    endpoint = 'https://api.github.com/graphql'
+#    query = """query {
+#      user(login: "torvalds"){
+#        contributionsCollection {
+#          endedAt
+#        }
+#      }
+#    }
+#    """
+#
+#    response = requests.post(endpoint, json={"query": query}, auth=auth)
+#    return response.json()
 
-    response = requests.post(endpoint, json={"query": query}, auth=auth)
+def get_data(user, token):
+    endpoint = 'https://api.github.com/search/commits?q=author:torvalds&sort=author-date&order=desc&page=1'
+    auth = HTTPBasicAuth(user, token)
+    response = requests.get(endpoint, auth=auth)
     return response.json()
 
 def get_date_from_response(response):
-    date = response['data']['user']['contributionsCollection']['endedAt']
+    #date = response['data']['user']['contributionsCollection']['endedAt']
     date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     return date
 
 def main():
-    pass
+    print(get_data(sys.argv[1], sys.argv[2]))
 
 
+if __name__ == '__main__':
+    main()
