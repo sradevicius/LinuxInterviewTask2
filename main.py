8e852f6aca4f55947f41793d03e839e1ea4c6708
Simon <sradevicius1@gmail.com>
2022-05-03 14:09:25 +0300

    Forked new branch to try different approach

--- main.py
+++ main.py
@@ -21,12 +21,7 @@ def send_mail():
     pass
 
 def get_data(user, token):
-    """Returns latest commits by Torvalds sorted by date. Two major notes, one is that github only 
-    indexes master branch, other is that some commits are in the far future"""
-    endpoint = 'https://api.github.com/search/commits?q=author:torvalds&sort=author-date&order=desc&page=1'
-    auth = HTTPBasicAuth(user, token)
-    response = requests.get(endpoint, auth=auth)
-    return response.json()
+    pass
 
 def get_datetime_from_date_string(date):
     #date = response['data']['user']['contributionsCollection']['endedAt']
@@ -42,23 +37,7 @@ def main():
     ## Some dates returned are far in the future, i.e. 2099 or 2038
     ## I also don't really care about commits done before 2022, so the below code
     ## Should get rid of those future commits in response
-    for i in range(len(data['items'])):
-        last_extracted_commit_date = data['items'][i]['commit']['author']['date']
-        if last_extracted_commit_date[:4] == '2022':
-            break
-    last_extracted_commit_date = get_datetime_from_date_string(last_commit_date)
-
-    if latest_commit_date == None:
-        latest_commit_date = last_extracted_commit_date
-    if latest_commit_date != last_extracted_commit_date:
-        print("Detected new commit!")
-        print("Old commit date was: ", latest_commit_date)
-        print("New commit date is: ", last_extracted_commit_date)
-        if latest_mail_send_date == None or latest_mail_send_date > latest_mail_send_date - datetime.timedelta(hours=24):
-            send_mail()
-            latest_mail_send_date = datetime.datetime.mow()
-    print(last_commit_date)
-
+    pass
 
 
 if __name__ == '__main__':
