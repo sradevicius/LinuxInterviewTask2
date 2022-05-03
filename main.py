6444ddc7e7f97c89baa9a80baa802bac48215718
Simon <sradevicius1@gmail.com>
2022-05-03 14:52:21 +0300

    Deleted commented out rubbish

--- main.py
+++ main.py
@@ -3,45 +3,10 @@ import sys
 from requests.auth import HTTPBasicAuth
 import datetime
 
-#def get_data(user, passwd):
-#    auth = HTTPBasicAuth(user, passwd)
-#    endpoint = 'https://api.github.com/graphql'
-#    query = """query {
-#      user(login: "torvalds"){
-#        contributionsCollection {
-#          endedAt
-#        }
-#      }
-#    }
-#    """
-#
-#    response = requests.post(endpoint, json={"query": query}, auth=auth)
-#    return response.json()
 def send_mail():
     pass
 
-
-def get_latest_push_date(user, token):
-    auth = HTTPBasicAuth(user, token)
-    endpoint = 'https://api.github.com/users/torvalds/events/public'
-    response = requests.get(endpoint, auth=auth)
-    for i in range(len(response.json())):
-        if response.json()[i]['type'] == 'PushEvent':
-            return response.json()[i]['created_at']
-    pass
-
-#def get_repo_list(auth):
-#    endpoint = 'https://api.github.com/users/torvalds/repos'
-#    response = requests.get(endpoint, auth=auth).json()
-#    repo_list = []
-#    for repo in range(len(response)):
-#        repo_list.append(response[repo]['url'])
-#    print(repo_list)
-#    return repo_list
-
 def get_datetime_from_date_string(date):
-    #date = response['data']['user']['contributionsCollection']['endedAt']
-    #date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     return date
 
