5e41915a5d188278b28e29b8892a1e35b5abe4eb
Simon <sradevicius1@gmail.com>
2022-05-03 12:20:21 +0300

    Added documentation string to get_data function to document two issues with it

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ import datetime
 #    return response.json()
 
 def get_data(user, token):
+    """Returns latest commits by Torvalds sorted by date. Two major notes, one is that github only 
+    indexes master branch, other is that some commits are in the far future"""
     endpoint = 'https://api.github.com/search/commits?q=author:torvalds&sort=author-date&order=desc&page=1'
     auth = HTTPBasicAuth(user, token)
     response = requests.get(endpoint, auth=auth)
@@ -30,7 +32,9 @@ def get_date_from_response(response):
     return date
 
 def main():
-    print(get_data(sys.argv[1], sys.argv[2])['items'][0]['commit'])
+    data = get_data(sys.argv[1], sys.argv[2])
+    last_commit_date = data['items'][0]['commit']['author']['date']
+    print(last_commit_date)
 
 
 if __name__ == '__main__':
